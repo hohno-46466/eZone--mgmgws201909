19371edbad80e0e90b5a122d5d4789606a3c5eed
hohno-46466 <hohno.46466@gmail.com>
2019-09-12 02:40:28 +0900

    updated by @hohno_at_kuimc for monogramming workshop 201909

---
+++ examples/20190913d_AnalogReadSerial_1POT/arduino/sketch20190913d_AnalogReadSerial_1POT/sketch20190913d_AnalogReadSerial_1POT.ino
@@ -0,0 +1,25 @@
+//
+// sketch_20190913_xxx.ino
+//
+// Written by @hohno_at_kuimc
+//
+// Public domain
+//
+// Last update: Mon Aug 19 07:28:04 JST 2019 by @hohno_at_kuimc
+//
+
+// ---------------------------------------------------------
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+// ---------------------------------------------------------
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// ---------------------------------------------------------
